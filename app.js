2844af384e255a9864e8fa2bc165ef6519496dad
jmartinez888 <jhonmartinez888@gmail.com>
2021-11-26 12:05:49 -0500

    Ampliacion de tamaño de recpcion del archivo json a 5Mb masximo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const { joiRegister } = require("./src/middlewares/core/joiRegister");
-const fileUpload = require("express-fileupload");
+// const { joiRegister } = require("./src/middlewares/core/joiRegister");
+// const fileUpload = require("express-fileupload");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -35,9 +35,9 @@ app.use(
 app.use(express.static("public"));
 
 // Lectura y parseo del body
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(fileUpload());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
+// app.use(fileUpload());
 
 // Rutas
 app.use("/api", require("./src/routes/index.routes"));
